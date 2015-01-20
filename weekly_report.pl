4f1dfb8c4e76cb51e5b1dfd81152c64ae38c98a9
Luciano Restifo <luciano.restifo@gmail.com>
2015-01-20 22:33:58 +0100

    First basic working version

--- weekly_report.pl
+++ weekly_report.pl
@@ -1,121 +1,82 @@
 #!/usr/bin/env perl -w
 ################################################################################
 #
-# Title:
-# Author:
+# Title:    WEEKLY_REPORT -- Query RT and build an XLSX report out of it
+# Author:   Tue Jan 20 21:09:45 CET 2015 lrestifo at esselte dot com
 # Description:
+#   This script executes the following tasks:
+#   1. Reads an SQL query from a text file
+#   2. Runs the SQL query against the RT database
+#   3. Saves query result set in an XLSX spreadsheet file
+#   4. Add formulas to the XLSX spreadheet thus building a complete report
+#   5. Saves the resulting XLSX report in a known location
+#   This script is designed to be run weekly, and receives the week number as
+#   a command-line parameter.  It defines some of the calculation formulas
+#   taking the current week into account, and also the resulting spreadsheet
+#   output file is named according to the given week
+# Usage:
+#   weekly_report [ yyyyww ]
 #
 ################################################################################
-#
+
 use strict;
 use DBI;
 use Excel::Writer::XLSX;
 
-# DATABASE - CUSTOMISE THESE
+# Database - Customize these
 #----------------------------
-my $host = 'localhost';
-my $db   = 'rtdb';
-my $user = 'rtuser';
-my $pass = 'rtpass';
+my $host = "localhost";
+my $db   = "rtdb";
+my $user = "rtuser";
+my $pass = "rtpass";
+
+# File names
+#------------
+my $sqlf = "./sql/sap_mp_alltickets.sql";
+my $xlsx = "./xls/tickets.xlsx";
 
 # Load the RT Query from the SQL source file
 #--------------------------------------------
 my $sql = "";
 my $lin = "";
-open(SQL, "<./sql/sap_mp_alltickets.sql") or die "Can't open ./sql/sap_mp_alltickets.sql: $!\n";
-while($lin = <SQL>) {
+open( SQL, "<$sqlf" ) or die "Can't open $sqlf: $!\n";
+while( $lin = <SQL> ) {
         $sql = $sql . $lin;
 }
 
-# connect to RT database
-#------------------------
-my $dbh = DBI->connect('dbi:mysql:'.$db.';host='.$host, $user, $pass) or die "Connection Error: $DBI::errstr\n";
+# Create the Excel workbook
+#---------------------------
+my $workbook = Excel::Writer::XLSX->new( $xlsx ) or die "Can't open $xlsx: $!\n";
+my $datasheet = $workbook->add_worksheet( "RawData" );
+my $rowno = 0;
+my $colno = 0;
+my @row;
+my @col;
+my $field;
 
-# connect to RT database
-#------------------------
-my $dbh = DBI->connect('dbi:mysql:'.$db.';host='.$host, $user, $pass) or die "Connection Error: $DBI::errstr\n";
-
-# run the query and save results as Excel
-#-----------------------------------------
-my $ss = Spreadsheet::WriteExcel::FromDB->read($dbh, $sql);
-$ss->write_xls('./data/rtdata.xls');
-
-# now reexecute, retrieve, output data
-#--------------------------------------
-my $sth = $dbh->prepare($sql) or die "Prepare Statement Error:" . $dbh->errstr . "\n";
-my $rows = $sth->execute() or die "SQL Execution Error:" . $sth->errstr . "\n";
+# Connect to RT database and run the query
+#------------------------------------------
+my $dbh = DBI->connect( "dbi:mysql:".$db.";host=".$host, $user, $pass ) or die "Connection error: " . $DBI::errstr . "\n";
+my $sth = $dbh->prepare( $sql )  or die "Prepare statement error: " . $dbh->errstr . "\n";
+my $rows = $sth->execute() or die "SQL execution error: " . $sth->errstr . "\n";
 
 # Tell field names apart
 #------------------------
-foreach $colno (0 .. $sth->{NUM_OF_FIELDS}) {
-        $col[$colno] = $sth->{NAME}->[$colno];
+foreach $colno( 0 .. $sth->{NUM_OF_FIELDS} ) {
+  $col[$colno] = $sth->{NAME}->[$colno];
+  $datasheet->write( $rowno, $colno, $col[$colno] );
 }
 
 # Let's go
 #----------
-open RTDATA, ">./data/rtdata.json" or die "Can't write ./data/rtdata.json: $!\n";
-open RTMONGO, ">./data/rtdata.mongoimport.json" or die "Can't write ./data/rtdata.mongoimport.json: $!\n";
-print RTDATA "[";
-while (@row = $sth->fetchrow_array())  {
-        # Output the entire row on both RTDATA and RTMONGO
-        print  RTDATA '{ ';
-        print RTMONGO '{ ';
-        foreach $field (0 .. $#row) {
-                print  RTDATA '"' . $col[$field] . '":';
-                print RTMONGO '"' . $col[$field] . '":';
-                if ( $ffmt{$col[$field]} =~ /\$date/ ) {
-                        print  RTDATA sprintf "new Date(%lu)", $row[$field] * 1000;
-                        print RTMONGO sprintf $ffmt{$col[$field]}, $row[$field] * 1000;
-                } else {
-                        if ( $ffmt{$col[$field]} =~ /\$subject/ ) {
-                                print  RTDATA $json->encode($row[$field]);
-                                print RTMONGO $json->encode($row[$field]);
-                        } else {
-                                print  RTDATA sprintf $ffmt{$col[$field]}, $row[$field];
-                                print RTMONGO sprintf $ffmt{$col[$field]}, $row[$field];
-                        }
-                }
-                if ($field < $#row) {
-                        print  RTDATA ', ';
-                        print RTMONGO ', ';
-                }
-        }
-        print RTMONGO " }\n";
-        print  RTDATA " }\n";
-        $created = DateTime->from_epoch( epoch => $row[$IX_CREA] );
-        $yy_index = $created->year();                                                           # e.g. 2012
-        $mm_index = 100 * $created->year() + $created->month();         # e.g. 201209
-        $rgn_index = $row[$IX_REGN];            # Region indicator
-        $ctg_index = $row[$IX_CATE];            # Problem category
-        $tkt_yy{$yy_index}{"Europe"}++;
-        $tkt_mm{$mm_index}{"Europe"}++;
-        $tkt_yy{$yy_index}{$rgn_index}++;
-        $tkt_mm{$mm_index}{$rgn_index}++;
-        $tkt_categ{$yy_index}{"Europe"}{$ctg_index}++;
-        $tkt_categ{$yy_index}{$rgn_index}{$ctg_index}++;
+while( @row = $sth->fetchrow_array() )  {
+  $rowno++;
+  foreach $field( 0 .. $#row ) {
+    $datasheet->write( $rowno, $field, $row[$field] );
+  }
 }
+
+# Game over
+#-----------
 $sth->finish;
-
-# ------
-
-
-# Create a new Excel workbook
-my $workbook = Excel::Writer::XLSX->new( 'perl.xlsx' );
-
-# Add a worksheet
-$worksheet = $workbook->add_worksheet();
-
-#  Add and define a format
-$format = $workbook->add_format();
-$format->set_bold();
-$format->set_color( 'red' );
-$format->set_align( 'center' );
-
-# Write a formatted and unformatted string, row and column notation.
-$col = $row = 0;
-$worksheet->write( $row, $col, 'Hi Excel!', $format );
-$worksheet->write( 1, $col, 'Hi Excel!' );
-
-# Write a number and a formula using A1 notation
-$worksheet->write( 'A3', 1.2345 );
-$worksheet->write( 'A4', '=SIN(PI()/4)' );
+$workbook->close() or die "Error closing $xlsx: $!\n";
